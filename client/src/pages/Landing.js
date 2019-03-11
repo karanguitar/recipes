6f187a4f587420efc82472fe305c80e918bf8ebe
Karan Dhanoya <kdhanoya94@gmail.com>
2019-03-11 14:21:24 +0000

    parallax effect added to home page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,6 +4,9 @@ const Landing = () => (
   <>
     <section className="landing__hero">
       <h1 className="landing__hero--heading">Recipes</h1>
+      <p className="landing__hero--subheading">
+        Create, discover and share your favourite recipes
+      </p>
     </section>
     <section className="landing__cards" />
   </>
